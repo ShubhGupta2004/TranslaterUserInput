35a12abc0c2595435a31bc0ab1b08293a6e5e01f
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-07-30 15:15:29 +0530

    commit 1.5

--- app/src/main/java/com/example/translateruserinput/MainActivity.java
+++ app/src/main/java/com/example/translateruserinput/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.translateruserinput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,35 @@ public class MainActivity extends AppCompatActivity {
         str.add("ello");
         str.add("hell");
         //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.listsize,ar);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.list_format,str);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,str);
         listView.setAdapter(arrayAdapter);
 
+        textView= findViewById(R.id.faimly);
+
+
+
+    }
+    public void fam(View v){
+        ArrayList<String> str = new ArrayList<>(3);
+        str.add("23");
+        str.add("helo");
+        str.add("90");
+        str.add("ello");
+        str.add("hell");
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.listsize,ar);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,str);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    public void parts(View v){
+        ArrayList<String> str = new ArrayList<>(3);
+        str.add("1");
+        str.add("2");
+        str.add("3");
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.listsize,ar);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,str);
+        listView.setAdapter(arrayAdapter);
 
     }
 }
