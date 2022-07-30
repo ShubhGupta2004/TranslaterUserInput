a371a469256fb40adf39becc0faae6b2ba51cd13
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-07-30 23:41:13 +0530

    commit 1.8

--- app/src/main/java/com/example/translateruserinput/MainActivity.java
+++ app/src/main/java/com/example/translateruserinput/MainActivity.java
@@ -3,41 +3,101 @@ package com.example.translateruserinput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    TextView textView;
+    private TextToSpeech mTts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i != TextToSpeech.ERROR){
+                    int result= mTts.setLanguage(Locale.getDefault());
+
+                    if(result==TextToSpeech.LANG_MISSING_DATA
+                            || result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        //Toast.makeText(this,"Language not supported",Toast.LENGTH_SHORT).show();
+                        Log.d("error1","Language Not supported");
+                    }else{
+                        Log.d("error1","Done ");
+                    }
+                }else{
+                    Log.d("error1","Language not intiated");
+                }
+            }
+        });
+
+
         listView= findViewById(R.id.list);
         ArrayList<dataSetter> arrayList = utilizePortion.extractData(0);
         adapter1 adapter = new adapter1(MainActivity.this,arrayList);
-        listView.setAdapter(adapter);
+        setAdapter(0);
+
+
     }
     public void fam(View v){
-        ArrayList<dataSetter> arrayList = utilizePortion.extractData(0);
-        adapter1 adapter = new adapter1(MainActivity.this,arrayList);
-        listView.setAdapter(adapter);
+        setAdapter(0);
+
     }
 
     public void parts(View v){
-        ArrayList<dataSetter> arrayList = utilizePortion.extractData(1);
-        adapter1 adapter = new adapter1(MainActivity.this,arrayList);
-        listView.setAdapter(adapter);
+        setAdapter(1);
     }
 
     public void Phrases(View v){
-        ArrayList<dataSetter> arrayList = utilizePortion.extractData(2);
+        setAdapter(2);
+    }
+
+    public void Names(View v) {
+        setAdapter(3);
+    }
+
+
+
+    public void setAdapter(int i){
+        ArrayList<dataSetter> arrayList = utilizePortion.extractData(i);
         adapter1 adapter = new adapter1(MainActivity.this,arrayList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                dataSetter currentPosition = adapter.getItem(i);
+                String s1 = currentPosition.getName1();
+                String s2 = currentPosition.getName2();
+                speak(s1,s2);
+            }
+        });
     }
+    private void speak(String s1,String s2){
+        mTts.speak(s1,TextToSpeech.QUEUE_FLUSH,null);
+        Log.d("error1","is speaking: "+mTts.isSpeaking());
+        mTts.speak(s2,TextToSpeech.QUEUE_ADD,null);
+    }
+
+    public void onPause(){
+        if(mTts != null){
+            mTts.stop();
+            mTts.shutdown();
+        }
+
+        super.onPause();
+    }
+
 }
--- app/src/main/java/com/example/translateruserinput/utilizePortion.java
+++ app/src/main/java/com/example/translateruserinput/utilizePortion.java
@@ -15,6 +15,10 @@ public final class utilizePortion {
                 arrayList.add(new dataSetter("Mother","2"));
                 arrayList.add(new dataSetter("Brother","3"));
                 arrayList.add(new dataSetter("Sister","4"));
+                arrayList.add(new dataSetter("father","5"));
+                arrayList.add(new dataSetter("Mother","6"));
+                arrayList.add(new dataSetter("Brother","7"));
+                arrayList.add(new dataSetter("Sister","8"));
                 return arrayList;
             case 1:
                 ArrayList<dataSetter> arrayList1 = new ArrayList<>();
@@ -22,6 +26,10 @@ public final class utilizePortion {
                 arrayList1.add(new dataSetter("stomach","2"));
                 arrayList1.add(new dataSetter("hands","3"));
                 arrayList1.add(new dataSetter("legs","4"));
+                arrayList1.add(new dataSetter("head","5"));
+                arrayList1.add(new dataSetter("stomach","6"));
+                arrayList1.add(new dataSetter("hands","7"));
+                arrayList1.add(new dataSetter("legs","8"));
                 return arrayList1;
             case 2:
                 ArrayList<dataSetter> arrayList2 = new ArrayList<>();
@@ -30,6 +38,14 @@ public final class utilizePortion {
                 arrayList2.add(new dataSetter("phrase","3"));
                 arrayList2.add(new dataSetter("phrase","4"));
                 return arrayList2;
+            case 3:
+                ArrayList<dataSetter> arrayList3 = new ArrayList<>();
+                arrayList3.add(new dataSetter("My name is Shashwat","1"));
+                arrayList3.add(new dataSetter("My name is Khushl","1"));
+                arrayList3.add(new dataSetter("My name is shubh","1"));
+                arrayList3.add(new dataSetter("My name is Kailash","1"));
+                arrayList3.add(new dataSetter("My name is vaishnavi","1"));
+                return arrayList3;
             default:
                 return null;
         }
