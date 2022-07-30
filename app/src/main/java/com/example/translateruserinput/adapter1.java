b5b077bc13456c0ce89c7a9fb0ff307a631fd2a7
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-07-30 16:14:21 +0530

    commit 1.6

---
+++ app/src/main/java/com/example/translateruserinput/adapter1.java
@@ -0,0 +1,40 @@
+package com.example.translateruserinput;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class adapter1 extends ArrayAdapter<dataSetter> {
+
+
+    public adapter1(@NonNull Context context, List<dataSetter> list) {
+        super(context, 0,list);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+
+        View listView = convertView;
+        if(listView==null){
+            listView= LayoutInflater.from(getContext()).inflate(R.layout.list_format,parent,false);
+
+        }
+
+        dataSetter dataSetterCurrent = getItem(position);
+
+        TextView name1 = listView.findViewById(R.id.name1);
+        TextView name2 = listView.findViewById(R.id.name2);
+
+        name1.setText(dataSetterCurrent.getName1());
+        name2.setText(dataSetterCurrent.getName2());
+
+        return listView;
+    }
+
+}
---
+++ app/src/main/java/com/example/translateruserinput/dataSetter.java
@@ -0,0 +1,21 @@
+package com.example.translateruserinput;
+
+public class dataSetter {
+    private final String name1;
+    private final String name2;
+
+    public dataSetter(String name1, String name2) {
+        this.name1 = name1;
+        this.name2 = name2;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+
+}
